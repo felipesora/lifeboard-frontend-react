cc98ec050a2951f42b391743716c406051c5d3dd
felipesora <felipeusora@gmail.com>
2025-07-07 22:25:50 -0300

    Criação e estilização da página de Login

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Login from './routes/Login/Login.jsx'
+
+
+const routes = createBrowserRouter([
+  {
+    path: '/',
+    element: <Login />,
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={routes} />
   </StrictMode>,
 )
---
+++ src/routes/Login/Login.jsx
@@ -0,0 +1,64 @@
+import "./Login.css";
+import Logo from "../../assets/images/logo-lifeboard-azul.png";
+import { useNavigate } from "react-router-dom";
+import React, { useState } from 'react';
+
+function Login() {
+
+    const [email, setEmail] = useState("");
+    const [senha, setSenha] = useState("");
+    const navigate = useNavigate();
+
+
+    return (
+        <div className="container_login">
+            <img src={Logo} alt="Logo do LifeBoard" />
+            <section className="container_login_form">
+                <div className="container_login_form_textos">
+                    <h1>Bem-Vindo de volta</h1>
+                    <p>Entre com sua conta</p>
+                </div>
+
+                <form>
+                    <div>
+                        <label htmlFor="">Email</label>
+                        <input 
+                            type="email" 
+                            placeholder="Digite seu email"
+                            required
+                            onChange={(e) => setEmail(e.target.value)}
+                            value={email}
+                            />
+                    </div>
+
+                    <div>
+                        <label htmlFor="">Senha</label>
+                        <input 
+                            type="password" 
+                            placeholder="Digite sua senha"
+                            required
+                            onChange={(e) => setSenha(e.target.value)}
+                            value={senha}
+                            />
+                    </div>
+
+                    <div>
+                        <button type="submit">Entrar</button>
+                    </div>
+
+                </form>
+
+                <div className="container_mensagem_login">
+                    {/* <p className="mensagem_login_sucesso">Login realizado com Sucesso!</p> */}
+                    {/* <p className="mensagem_login_erro">Email ou senha incorretos!</p> */}
+                </div>
+
+                <div className="container_link_cadastro">
+                    <p className="link_cadastro">Ainda não possui uma conta? <button onClick={() => navigate("/cadastro")}>Cadastre-se</button></p>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default Login;
