c2bea4288a1fddba6a161d0b62f0b327c545e2a8
felipesora <felipeusora@gmail.com>
2025-07-09 22:24:20 -0300

    Criação do card de Minhas metas na pagina de controle financeiro

--- src/routes/ControleFinanceiro/ControleFinanceiro.jsx
+++ src/routes/ControleFinanceiro/ControleFinanceiro.jsx
@@ -5,6 +5,7 @@ import IconeSalario from "../../assets/images/icone-salario.png";
 import IconeGastos from "../../assets/images/icone-cartao-credito.png";
 import IconeTransacoes from "../../assets/images/icone-lista-preto.png";
 import IconeGrafico from "../../assets/images/icone-grafico.png";
+import IconeMetas from "../../assets/images/icone-metas-preto.png";
 import "./controleFinanceiro.css";
 import CardTrasacao from "../../components/CardTransacao/CardTransacao";
 import GastosMensaisGrafico from "../../components/GastosMensaisGrafico/GastosMensaisGrafico";
@@ -30,81 +31,91 @@ function ControleFinanceiro() {
         <div className="dashboard_container">
             <MenuLateral />
             <main className="dashboard_main">
-                <p>Controle financeiro</p>
-                <div className="dashboard_cards_container">
-                    <div className="dashboard_cards_pequenos_graficos">
-                        <div className="dashboard_cards_pequenos">
-                            <CardPequeno
-                                icone={IconeSaldo}
-                                descricao="Icone sacola de dinheiro"
-                                titulo="Saldo Atual"
-                                valor="7.000,00"
-                                cor="#000000"
-                            />
-                            <CardPequeno
-                                icone={IconeSalario}
-                                descricao="Icone notas de dinheiro"
-                                titulo="Salário"
-                                valor="10.000,00"
-                                cor="#2E7D32"
-                            />
-                            <CardPequeno
-                                icone={IconeGastos}
-                                descricao="Icone cartão de crédito"
-                                titulo="Gastos do mês"
-                                valor="5.000,00"
-                                cor="#A44A48"
-                            />
-                        </div>
+                <p>Controle Financeiro</p>
 
-                        <div className="dashboard_grafico">
-                            <div className="card_grafico_titulo">
-                                <img src={IconeGrafico} alt="Icone de grafico" />
-                                <p>Gráficos</p>
+                <div>
+                    <div className="dashboard_cards_container">
+                        <div className="dashboard_cards_pequenos_graficos">
+                            <div className="dashboard_cards_pequenos">
+                                <CardPequeno
+                                    icone={IconeSaldo}
+                                    descricao="Icone sacola de dinheiro"
+                                    titulo="Saldo Atual"
+                                    valor="7.000,00"
+                                    cor="#000000"
+                                />
+                                <CardPequeno
+                                    icone={IconeSalario}
+                                    descricao="Icone notas de dinheiro"
+                                    titulo="Salário"
+                                    valor="10.000,00"
+                                    cor="#2E7D32"
+                                />
+                                <CardPequeno
+                                    icone={IconeGastos}
+                                    descricao="Icone cartão de crédito"
+                                    titulo="Gastos do mês"
+                                    valor="5.000,00"
+                                    cor="#A44A48"
+                                />
                             </div>
-                            <GastosMensaisGrafico dados={data} />
+                            <div className="dashboard_grafico">
+                                <div className="card_grafico_titulo">
+                                    <img src={IconeGrafico} alt="Icone de grafico" />
+                                    <p>Gráficos</p>
+                                </div>
+                                <GastosMensaisGrafico dados={data} />
+                            </div>
+                        </div>
+                        <div className="card_transacoes">
+                            <div>
+                                <img src={IconeTransacoes} alt="Icone de lista" />
+                                <p>Últimas Transações</p>
+                            </div>
+                            <CardTrasacao
+                                corFundo="#FFEBEE"
+                                corTexto="#A44A48"
+                                titulo="Saída"
+                                valor="300,00"
+                                descricao="Mercado"
+                                data="06/07/2025"
+                            />
+                            <CardTrasacao
+                                corFundo="#E8F5E9"
+                                corTexto="#2E7D32"
+                                titulo="Entrada"
+                                valor="2.500,00"
+                                descricao="Salário"
+                                data="06/07/2025"
+                            />
+                            <CardTrasacao
+                                corFundo="#FFEBEE"
+                                corTexto="#A44A48"
+                                titulo="Saída"
+                                valor="10,00"
+                                descricao="Transporte"
+                                data="06/07/2025"
+                            />
+                            <CardTrasacao
+                                corFundo="#E8F5E9"
+                                corTexto="#2E7D32"
+                                titulo="Entrada"
+                                valor="2.500,00"
+                                descricao="Salário"
+                                data="06/06/2025"
+                            />
+                            <div>
+                                <button>Ver mais...</button>
+                            </div>
                         </div>
                     </div>
-                    <div className="card_transacoes">
-                        <div>
-                            <img src={IconeTransacoes} alt="Icone de lista" />
-                            <p>Últimas Transações</p>
+                    <div className="card_metas">
+                        <div className="card_metas_titulo">
+                            <img src={IconeMetas} alt="Icone de cofrinho" />
+                            <p>Minhas metas</p>
                         </div>
-                        <CardTrasacao
-                            corFundo="#FFEBEE"
-                            corTexto="#A44A48"
-                            titulo="Saída"
-                            valor="300,00"
-                            descricao="Mercado"
-                            data="06/07/2025"
-                        />
-                        <CardTrasacao
-                            corFundo="#E8F5E9"
-                            corTexto="#2E7D32"
-                            titulo="Entrada"
-                            valor="2.500,00"
-                            descricao="Salário"
-                            data="06/07/2025"
-                        />
-                        <CardTrasacao
-                            corFundo="#FFEBEE"
-                            corTexto="#A44A48"
-                            titulo="Saída"
-                            valor="10,00"
-                            descricao="Transporte"
-                            data="06/07/2025"
-                        />
+                        <div className="card_metas_lista">
 
-                        <CardTrasacao
-                            corFundo="#E8F5E9"
-                            corTexto="#2E7D32"
-                            titulo="Entrada"
-                            valor="2.500,00"
-                            descricao="Salário"
-                            data="06/06/2025"
-                        />
-                        <div>
-                            <button>Ver mais...</button>
                         </div>
                     </div>
                 </div>
